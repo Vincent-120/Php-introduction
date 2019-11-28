7c82442af018d143c1fe1fa8f93604df645beb62
Vincent-120 <v1996.vincent@gmail.com>
2019-11-28 10:08:28 +0100

    add introduction

---
+++ Introduction/cats.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<?php
+$titre_page='Acceuil';
+include('http://localhost:8888/php/test/index.php');
+
+echo("
+<td rowspan=\"2\" bgcolor=\"#FFFFFF\">
+<img src=\"cat.jpg\" width=\"100\" height=\"100\"/>
+</td>");
+
+
+include('http://localhost:8888/php/test/index.php');
+?>
+</body>
+</html> 
