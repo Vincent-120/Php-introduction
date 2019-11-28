21a8f4a2980988f46d5e397bf3857137d3c0ebfa
Vincent-120 <v1996.vincent@gmail.com>
2019-11-28 10:29:57 +0100

    add exo variable

---
+++ variable/variable_type.php
@@ -0,0 +1,15 @@
+<html>
+  <head><title>Hi!</title></head>
+  <body>
+    <?php 
+      $name = 'Vincent';
+      $firstName = 'Vissers';
+      $age = 23;
+      $eyes = 'brown';
+    ?>
+    <p>Hi! My name is <?php echo $name ?></p>
+    <p> I am <?php echo $age ?> years old. </p>
+    <p>My eyes are <?php echo $eyes ?></p>
+    <p>The first person in my family is <?php echo $firstName ?></p>
+  </body>
+</html>
